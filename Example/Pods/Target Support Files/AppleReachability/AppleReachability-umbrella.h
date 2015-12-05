818eb52999e32be6d67c4ce4cbffc8dc7bd9505e
Antoine Cœur <coeur@gmx.fr>
2015-12-05 15:40:05 +0800

    fix warnings

--- Example/Pods/Target Support Files/AppleReachability/AppleReachability-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Reachability.h"
-
-FOUNDATION_EXPORT double AppleReachabilityVersionNumber;
-FOUNDATION_EXPORT const unsigned char AppleReachabilityVersionString[];
-
